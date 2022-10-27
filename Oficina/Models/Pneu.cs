3d679cbb55f277cd2970f0faabc1b85aa37d2661
André Victor <115494201+Atorvic@users.noreply.github.com>
2022-10-27 17:06:35 -0300

    Criando a Oficina

---
+++ Oficina/Models/Pneu.cs
@@ -0,0 +1,33 @@
+﻿namespace Models
+{
+    public class Pneu
+    {
+        private string marca;
+        private double dureza;
+        private double aro;
+        private bool furado;
+        
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+        public double Dureza
+        {
+            get { return dureza; }
+            set { dureza = value; }
+        }
+        public double Aro
+        {
+            get { return aro; }
+            set { aro = value; }
+        }
+        public bool Furado
+        {
+            get { return furado; }
+            set { furado = value; }
+        }
+
+
+    }
+}
