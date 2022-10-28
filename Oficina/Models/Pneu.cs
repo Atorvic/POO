5ba0e04e215e4baa7d3107f6cf301bff9327aeba
André Victor <115494201+Atorvic@users.noreply.github.com>
2022-10-28 16:40:16 -0300

    Atualização da oficina

--- Oficina/Models/Pneu.cs
+++ Oficina/Models/Pneu.cs
@@ -1,40 +1,125 @@
 ﻿namespace Models
 {
-    public class Pneu
-    {
-        private string marca;
-        private double dureza;
+        public class Pneu
+        {
         private int aro;
+        private string marca;
         private bool furado;
-        
-        public string Marca
+        private int psi;
+        private bool estourado;
+        private double percentualBorracha;
+        private int rotacao;
+        private Carro parar;
+
+        public Carro Parar
         {
-            get { return marca; }
-            set { marca = value; }
+            get { return parar; }
+            set { parar = value; }
         }
-        public double Dureza
+
+
+        public int Rotacao
         {
-            get { return dureza; }
-            set { dureza = value; }
+            get { return rotacao; }
+            set { rotacao = value; }
         }
-        public int Aro
+
+
+        public double PercentualBorracha
         {
-            get { return aro; }
-            set { aro = value; }
+            get { return percentualBorracha; }
+            set { percentualBorracha = value; }
         }
+
+        public bool Estourado
+        {
+            get { return estourado; }
+            set { estourado = value; }
+        }
+
+        public int Psi
+        {
+            get { return psi; }
+            set { psi = value; }
+        }
+
         public bool Furado
         {
             get { return furado; }
             set { furado = value; }
         }
-        public Pneu(string _marca, int _aro)
+
+        public string Marca
         {
-            Marca = _marca;
-            Dureza = 100;
-            Aro = _aro;
-            Furado = false;
+            get { return marca; }
+            set { marca = value; }
         }
 
-
+        public int Aro
+        {
+            get { return aro; }
+            set { aro = value; }
+        }
+        public Pneu(int aro, string marca)
+        {
+            Aro = aro;
+            Marca = marca;
+            Furado = false;
+            Psi = 33;
+            Estourado = false;
+            PercentualBorracha = 100;
+            Rotacao = 0;
+        }
+        public void Girar(int _rotacao)
+        {
+            if (Furado || Estourado)
+                return;
+           
+            Rotacao += _rotacao;
+            GastarBorracha(_rotacao / 100.0);
+            
+        }
+        public void Frear(int _abatimento)
+        {
+            Rotacao -= _abatimento;
+            GastarBorracha(_abatimento / 100.0);
+            if(Rotacao < 0)
+            {
+                Rotacao = 0;
+            }
+        }
+        public void Furar()
+        {
+            Furado = true;
+            Psi = 0;
+        }
+        public void Remendar()
+        {
+            Furado = false;
+            Psi = 33;
+        }
+        private void GastarBorracha(double _quantidade)
+        {
+            PercentualBorracha -= _quantidade;
+            if (PercentualBorracha <= 0)
+                Estourar();
+        }
+        private void Estourar()
+        {
+            Estourado = true;
+            Psi = 0;
+            PercentualBorracha = 0;
+            Rotacao = 0;
+        }
+        public void Exibir()
+        {
+            Console.WriteLine("Aro: " + Aro);
+            Console.WriteLine("Marca: " + Marca);
+            Console.WriteLine("Furado: " + Furado);
+            Console.WriteLine("Psi: " + Psi);
+            Console.WriteLine("Estourado: " + Estourado);
+            Console.WriteLine("Percentual de borracha: " + PercentualBorracha);
+            Console.WriteLine("Rotação: " + Rotacao);
+        }
     }
 }
--- Oficina/Oficina/Program.cs
+++ Oficina/Oficina/Program.cs
@@ -1,7 +1,22 @@
-﻿internal class Program
+﻿using Models;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-    }
+        Models.Carro corcel = new Models.Carro("Chevrolet", "2");
+        corcel.LigarDesligar();
+        
+        for(int i=0; i <= 4; i++)
+        {
+            corcel.Acelerar(120);
+            corcel.Frear(120);
+        }
+
+
+
+        corcel.Exibir();
+        
+
+     }
 }
